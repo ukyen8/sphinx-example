544e913a1dc88b51a57de64a9d49de6f3a4689f1
Shawnice <wucean@gmail.com>
2022-08-13 23:15:44 +0100

    refactor: extract source dir

--- docs_generator.sh
+++ docs_generator.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 MODULE_PATH=$1
+SOURCE_DIR=docs/source
+BUILD_DIR=docs/build
 
 # Generate source files from module path
-sphinx-apidoc -f -o docs/source $MODULE_PATH
+sphinx-apidoc -f -o $SOURCE_DIR $MODULE_PATH
 
 # Insert source file name
-sed "s/Contents:/Contents:\n\n   $MODULE_PATH/g" docs/source/index.tmpl.rst > docs/source/index.rst
+sed "s/Contents:/Contents:\n\n   $MODULE_PATH/g" $SOURCE_DIR/index.tmpl.rst > $SOURCE_DIR/index.rst
 
 # Generate html documentation from source files
-sphinx-build -b html docs/source docs/build
+sphinx-build -b html $SOURCE_DIR $BUILD_DIR
